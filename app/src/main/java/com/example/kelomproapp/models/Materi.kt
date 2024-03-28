3822a56cb5dc6aad9ff2f30076257ee6f9bd4700
Rian Farhan <rian.farhan99@gmail.com>
2024-03-29 00:10:51 +0800

    materi1

--- app/src/main/java/com/example/kelomproapp/models/Materi.kt
+++ app/src/main/java/com/example/kelomproapp/models/Materi.kt
@@ -1,4 +1,14 @@
 package com.example.kelomproapp.models
 
-class Materi {
-}
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import java.util.*
+
+@Parcelize
+data class Materi (
+    val id : String? = UUID.randomUUID().toString(),
+    val name : String = "",
+    val courses: String = "",
+    val topic: String = ""
+
+        ) : Parcelable
--- app/src/main/java/com/example/kelomproapp/ui/activities/MateriActivity.kt
+++ app/src/main/java/com/example/kelomproapp/ui/activities/MateriActivity.kt
@@ -2,10 +2,17 @@ package com.example.kelomproapp.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kelomproapp.R
+import com.example.kelomproapp.databinding.ActivityMateriBinding
 
 class MateriActivity : AppCompatActivity() {
+    private var binding : ActivityMateriBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMateriBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_materi)
+        setContentView(binding?.root)
     }
+
+
 }
